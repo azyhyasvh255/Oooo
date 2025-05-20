5dc3927340c2957fa8d6a3eeb9314367d24c49f3
azyhyasvh255 <iwjnashabshsj@gmail.com>
2025-05-20 07:36:36 +0530

    Update Jsons.lua

--- Jsons.lua
+++ Jsons.lua
@@ -1,44 +1,23 @@
-getgenv().simple_settings = {
-    ["MASTERY"] = { -- Settings related to leveling up weapon or skill mastery
-        ["ACTIVE"] = true, -- Enable or disable mastery leveling (true = enabled, false = disabled)
-        ["METHOD"] = "Half", -- Method for gaining mastery, "Half"[350] or "Full"[600]
-    },
+-- Faster Auto Clicker Script
+-- CPS: 50 (Clicks Per Second)
+-- Toggle: CAPSLOCK ON
+-- Mouse Button: 5 (Usually side button)
 
-    ["OBJECTIVE"] = { -- Goals for farming and unlocking features
-        ["GODHUMAN"] = true, -- Automatically unlock the "Godhuman" fighting style
-        ["RACE-CONFIGURE"] = {
-            ["RACE"] = {"Human", "Skypiea", "Fishman", "Mink"}, -- List -- "Human", "Skypiea", "Fishman", "Mink"
-            ["RACE-LOCK"] = true, -- Automatically change the character race if not in the list
-            ["RACE-V3"] = true, -- Automatically upgrade character race to V3 if possible Human, Mink, (Fishman, Ghoul, Cyborg) soon
-        },
-        ["FRAGMENT"] = 30000, -- Limit number of fragments to collect
+local CPS = 50 -- Higher clicks per second for more speed
+local keyToggle = "capslock" -- ON/OFF toggle key
+local keyMouse = 5 -- Button that must be held for clicking
 
-        -- SWORD
-        ["CANVANDER"] = true,
-        ["BUDDY-SWORD"] = true,
-        ["CURSED-DUAL-KATANA"] = true,
-        ["SHARK-ANCHOR"] = true,
+EnablePrimaryMouseButtonEvents(true)
 
-        --GUN
-        ["ACIDUM-RIFLE"] = true,
-        ["VENOM-BOW"] = true,
-        ["SOUL-GUITAR"] = true,
-
-        -- AURA
-        ["COLOR-HAKI"] = {"Pure Red","Winter Sky","Snow White"}, -- Aura color to craft
-    },
-
-    ["FRUITPURCHASE"] = true, -- Automatically purchase fruits based on priority list
-    ["PRIORITYFRUIT"] = { -- List of preferred fruits to purchase or eat in order of priority
-        [1] = "Dragon-Dragon",
-        [2] = "Dough-Dough",
-        [3] = "Flame-Flame",
-        [4] = "Rumble-Rumble",
-        [5] = "Human-Human: Buddha",
-        [6] = "Dark-Dark",
-    },
-
-    ["FPSCAP"] = 30, -- Limit the frame rate to optimize performance
-    ["LOWTEXTURE"] = true-- Reduce graphic quality for better performance
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/simple-hubs/contents/refs/heads/main/bloxfruit-kaitan-main.lua"))()
+function OnEvent(event, arg)
+  if IsKeyLockOn(keyToggle) then
+    if IsMouseButtonPressed(keyMouse) then
+      repeat
+        local time = math.floor(1000 / CPS) -- Time per click in ms
+        PressMouseButton(1)
+        ReleaseMouseButton(1)
+        Sleep(time)
+      until not IsMouseButtonPressed(keyMouse)
+    end
+  end
+end
